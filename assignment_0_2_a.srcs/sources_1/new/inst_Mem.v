b362a8e53b56db2ceb787bdcece0577b2bb5f02a
Juan Orlando <jcorlandomc@yahoo.com>
2020-08-20 15:56:16 -0700

    8/20/2020 3:56 pm

--- assignment_0_2_a.srcs/sources_1/new/inst_Mem.v
+++ assignment_0_2_a.srcs/sources_1/new/inst_Mem.v
@@ -1,8 +1,16 @@
 `timescale 1ns / 1ps
 
 
-module inst_Mem();
+module inst_Mem # (parameter WL_mem = 32, MEM_depth = 32)
+                  ( input wire [31:0] addr,
+                    output wire [31:0] instruction );
 
+    reg [WL_mem - 1 : 0] rom[ 0 : MEM_depth - 1];
+    
+    
+
+    assign instruction = rom[addr];
 
 endmodule
+
 
