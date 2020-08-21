33f189422269b548320d84080ef774d74e761f7c
Juan Orlando <jcorlandomc@yahoo.com>
2020-08-20 20:54:51 -0700

    8/20/2020 8:54 pm

--- assignment_0_2_a.srcs/sim_1/new/tb_inst_Mem.v
+++ assignment_0_2_a.srcs/sim_1/new/tb_inst_Mem.v
@@ -9,7 +9,14 @@ wire [31 : 0] instruction;
 
 inst_Mem DUT(.addr(addr), .instruction(instruction));
 
-initial addr = 11;
+initial
+begin
+    addr = 1;
+    # 200 addr = 8;
+    # 200 addr = 11;
+    # 200 addr = 31;
+    # 200 addr = 0;
+end
 
 
 
