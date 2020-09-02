a4f970f1827c06af72b1a6b72e858fb59d950598
Juan Orlando <jcorlandomc@yahoo.com>
2020-09-02 12:04:30 -0700

    9/02/2020 12:04 pm

--- assignment_0_2_a.srcs/sim_1/new/tb_inst_Mem.v
+++ assignment_0_2_a.srcs/sim_1/new/tb_inst_Mem.v
@@ -2,12 +2,15 @@
 
 module tb_inst_Mem;
 
+// Parameters
+parameter WL_mem = 32, MEM_depth = 32;
 //Inputs
 reg [31 : 0] addr;
 //Outputs
 wire [31 : 0] instruction;
 
-inst_Mem DUT(.addr(addr), .instruction(instruction));
+inst_Mem # ( .WL_mem(WL_mem), .MEM_depth(MEM_depth) )
+            DUT ( .addr(addr), .instruction(instruction) );
 
 initial
 begin
--- assignment_0_2_a.srcs/sources_1/new/inst_Mem.v
+++ assignment_0_2_a.srcs/sources_1/new/inst_Mem.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 
 module inst_Mem # (parameter WL_mem = 32, MEM_depth = 32)
-                  ( input [31:0] addr,
-                    output wire [31:0] instruction );
+                  ( input [MEM_depth - 1 : 0] addr,
+                    output wire [WL_mem - 1 : 0] instruction );
 
     reg [WL_mem - 1 : 0] rom[ 0 : MEM_depth - 1];
 
